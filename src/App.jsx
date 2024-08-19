c3f6cfd98a92f240ada8beda16ce616197e8b9fc
Jason <murangira99@gmail.com>
2024-08-19 21:51:33 +0200

    removed path index

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { Route } from 'express';
-import './App.css'
-import  Home from './Pages/Home'
-import Login from './Pages/Login'
+import { Route } from 'react-router-dom';
+import './App.css';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from "react-router-dom";
-
-
-
+} from 'react-router-dom';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
-    <Route index element={<Login/>}/>
-    <Route path ='Home' element={<Home/>}/>
-  </>
-  )
-
-
-)
-
-
-function App() {
-
-  return (
-    <>
-    <RouterProvider router={router}/>
+      <Route index element={<Login/>}/>
+      <Route path='/Home' element={<Home/>}/>
     </>
   )
+);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  );
 }
 
-export default App
+export default App;
+
