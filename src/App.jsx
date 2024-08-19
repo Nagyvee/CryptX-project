0eedea5dbf7532b0bf406466169e5290462ea956
Jason <murangira99@gmail.com>
2024-08-19 21:19:09 +0200

    added rouetes bath

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { Route } from 'express';
 import './App.css'
 import  Home from './Pages/Home'
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
 } from "react-router-dom";
+
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+    <Route index element={<Home/>}/>
+  </>
+  )
+
+
+)
 
 
 function App() {
