8e1c25e127c08742108d2a63010c35f1c0131380
Jason <murangira99@gmail.com>
2024-08-19 15:16:47 +0200

    delted app.tsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,13 @@
 import './App.css'
 import  Home from './Pages/Home'
 
+function App() {
 
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-// Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  return (
+    <>
+    <Home />
+    </>
+  )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <Home />
-    </ClerkProvider>
-  </React.StrictMode>,
-)
-
+export default App
