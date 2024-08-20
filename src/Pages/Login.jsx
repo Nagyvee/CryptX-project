c4288ecb0ed0419e34aace7e910c55baf98a21aa
Jason <murangira99@gmail.com>
2024-08-20 22:15:56 +0200

    removed headrs ds

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,19 @@
+
 import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
 
 export default function Login() {
   return (
     <div>Login</div>
   )
 }
+
+    <header>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </header>
+  
