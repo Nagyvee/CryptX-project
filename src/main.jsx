ec09770198846a8d328c2f5cf417c48d6641c1d9
Jason <murangira99@gmail.com>
2024-08-20 22:17:00 +0200

    removed crrate tootd

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+CreateRoot(document.getElementById('root')).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
