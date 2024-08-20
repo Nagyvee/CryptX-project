a1794f9b6cc0d9dcac1110e3c0e36b2f4c5f1255
Jason <murangira99@gmail.com>
2024-08-20 22:01:00 +0200

    trmoved dticl mode

--- src/main.jsx
+++ src/main.jsx
@@ -14,9 +14,9 @@ if (!PUBLISHABLE_KEY) {
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <App />
     </ClerkProvider>
-  </React.StrictMode>,
+  </StrictMode>,
 )
